c94c98fec1b9989b52791431b0b5962fb254bdf0
Japhet MPELE <54818089+japhetmpele1@users.noreply.github.com>
2026-04-03 06:36:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from simulation import run_simulation
+from simulation import run_simulation, plot_results
 
 if __name__ == "__main__":
     results = run_simulation(num_vehicles=20, steps=100)
-    print(results)
+
+    print("Total collisions:", results["total_collisions"])
+
+    plot_results(results["history"])
